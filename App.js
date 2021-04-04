684c0df5bd96470cfb559f0743ce4dfd252d3f61
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2021-04-04 20:59:19 +0530

    create and use custom component

--- App.js
+++ App.js
@@ -1,16 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Here you define/create a user define custom component
+const CustomComponent1 = () =>{
+  return(
+    <view>
+      <Text>Your CustomComponent 1</Text>
+    </view>
+  );
+}
+
+const CustomComponent2 = () =>{
+  return(
+    <view>
+      <Text>Your CustomComponent 2</Text>
+    </view>
+  );
+}
+
+// Root component .........................................................................
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      {/*Here we use our custom component*/}
+      <CustomComponent1/>
+      <CustomComponent2/>
     </View>
   );
 }
 
+// .........................................................................................
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ README.md
@@ -0,0 +1,28 @@
+# React Native Custom Component
+
+## Custom Component : 
+<p>Every component name should be start with <b>Capital latter</b></p>
+
+```js
+const CustomComponent1 = () =>{
+return <Text>Your CustomComponent 1</Text>
+} 
+```
+<p>Using this component into our code then just put it as a self closing tag</p>
+
+```js
+<CustomComponent1/>
+```
+## Usage
+```
+ $ git clone https://github.com/CybeXTron/React-Native-Expo-CustomComponent.git
+ $ cd React-Native-Expo-CustomComponent
+ $ npm install 
+ $ npm run [web, android, ios]
+ 
+```
+## Run with Expo 
+```
+ $ expo start --[web, android, ios]
+
+```
