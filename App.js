00ea02d36820852865713d4c263836b692eb32cb
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2021-07-11 21:42:25 +0530

    update 1.0

--- App.js
+++ App.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 // Here you define/create a user define custom component
-const CustomComponent1 = () =>{
-  return(
+const CustomComponent1 = () => {
+  return (
     <view>
       <Text>Your CustomComponent 1</Text>
     </view>
   );
 }
 
-const CustomComponent2 = () =>{
-  return(
+const CustomComponent2 = () => {
+  return (
     <view>
       <Text>Your CustomComponent 2</Text>
     </view>
@@ -19,18 +19,16 @@ const CustomComponent2 = () =>{
 }
 
 // Root component .........................................................................
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      
       {/*Here we use our custom component*/}
-      <CustomComponent1/>
-      <CustomComponent2/>
+      <CustomComponent1 />
+      <CustomComponent2 />
     </View>
   );
 }
-
-// .........................................................................................
+export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
